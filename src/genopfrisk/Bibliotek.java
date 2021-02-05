251079fa5f95e4b1033bc3f1b8fbb07607d13ebb
SePe19 <72380247+SePe19@users.noreply.github.com>
2021-02-05 15:23:14 +0100

    Fourth

---
+++ src/genopfrisk/Bibliotek.java
@@ -0,0 +1,31 @@
+package genopfrisk;
+
+import java.util.ArrayList;
+
+public class Bibliotek {
+    private static ArrayList<Bog> bibliotek = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Bog paranoia = new Bog(1234567011,"Paranoia",1999);
+        Bog peterPedal = new Bog(1234567091,"Peter Pedal",1992);
+        Bog pippiLangstrømpe = new Bog(1234591411,"Pippi på eventyr",1962);
+        bibliotek.add(paranoia);
+        bibliotek.add(peterPedal);
+        bibliotek.add(pippiLangstrømpe);
+        System.out.println(objektISBN(bibliotek, paranoia));
+        System.out.println(objektISBN(bibliotek, peterPedal));
+        System.out.println(objektISBN(bibliotek, pippiLangstrømpe));
+    }
+
+    public static boolean objektISBN(ArrayList<Bog> b, Bog bog){
+        for (int i = 0; i < b.size(); i++) {
+            if(bog.getISBNNummer() == b.get(i).getISBNNummer()){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/genopfrisk/Bog.java
@@ -0,0 +1,47 @@
+package genopfrisk;
+
+public class Bog {
+    private int iSBNNummer;
+    private String titel;
+    private int udgivelsesaar;
+
+    public Bog(int iSBNNummer, String titel, int udgivelsesaar) {
+        this.iSBNNummer = iSBNNummer;
+        this.titel = titel;
+        this.udgivelsesaar = udgivelsesaar;
+    }
+
+    public int getISBNNummer() {
+        return iSBNNummer;
+    }
+
+    public void setISBNNummer(int iSBNNummer) {
+        this.iSBNNummer = iSBNNummer;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public int getUdgivelsesaar() {
+        return udgivelsesaar;
+    }
+
+    public void setUdgivelsesaar(int udgivelsesaar) {
+        this.udgivelsesaar = udgivelsesaar;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Bog{" +
+                "iSBNNummer='" + iSBNNummer + '\'' +
+                ", titel='" + titel + '\'' +
+                ", udgivelsesaar=" + udgivelsesaar +
+                '}';
+    }
+}
